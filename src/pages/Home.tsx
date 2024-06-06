e3a45d4ef47fbc33f5bd9b6cdc79d696a5d2a965
Henrique Damasceno <henriquim25@yahoo.com.br>
2024-06-06 12:49:44 -0300

    feat:home implemented

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,33 @@
 import { FC } from "react";
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div className="container mx-auto p-4 pt-6 md:p-6 lg:p-12">
+      <h1 className="text-3xl font-bold underline bg-slate-900">
+        Finance Manager App
+      </h1>
+      <p className="text-lg">Bem vindo ao seu app de finanças</p>
+      <div className="flex flex-wrap justify-center mt-6">
+        <div className="w-full flex-1 p-6 box-sizing:border-box">
+          <div className="rounded-md bg-slate-800 p-3">
+            <h2 className="text-lg font-bold">Renda</h2>
+            <p className="text-gray-600">
+              Veja suas receitas e controle seus ganhos.
+            </p>
+          </div>
+        </div>
+        <div className="w-full flex-1 p-6 box-sizing:border-box">
+          <div className="rounded-md bg-slate-800 p-3">
+            <h2 className="text-lg font-bold">Despesas</h2>
+            <h1 className="text-gray-600">
+              Descubra para onde seu dinheiro está indo. Tenha um controle
+              melhor sobre seus gastos.
+            </h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
